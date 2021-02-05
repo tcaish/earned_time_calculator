b1324ca74c48f3888e37129c4b051d9bdf56c7e8
tcaish <77754475+tcaish@users.noreply.github.com>
2021-02-05 11:24:48 -0500

    added css for the logo so it doesn't put the navigation on a separate line; updated the body font to rockout; added conditional rendering for the components if profile was filled out

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 // React bootstrap
-import { Container, Row, Col, Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 // Styles
 import '../styles/App.css';
@@ -22,68 +22,36 @@ function Summary({ summary }) {
             <td className="align-center align-middle">
               ET Rate (per pay period)
             </td>
-            <td>
+            <td className="align-middle">
               <h4>{summary.et_rate} hrs</h4>
             </td>
           </tr>
           <tr key={1} className="align-center">
             <td className="align-center align-middle">ET at End of Year</td>
-            <td>
+            <td className="align-middle">
               <h4>{summary.et_end_of_year} hrs</h4>
             </td>
           </tr>
           <tr key={2} className="align-center">
             <td className="align-center align-middle">ET/Holiday to Burn</td>
-            <td>
+            <td className="align-middle">
               <h4>{summary.et_hol_to_burn} hrs</h4>
             </td>
           </tr>
           <tr key={3} className="align-center">
             <td className="align-center align-middle">Total Vacation Weeks</td>
-            <td>
+            <td className="align-middle">
               <h4>{summary.total_vaca_weeks}</h4>
             </td>
           </tr>
           <tr key={4} className="align-center">
             <td className="align-center align-middle">Total Vacation Days</td>
-            <td>
+            <td className="align-middle">
               <h4>{summary.total_vaca_days}</h4>
             </td>
           </tr>
         </tbody>
       </Table>
-
-      {/* <h3 className="align-center">Summary</h3>
-      <Row>
-        <Col xs={8} md={8} className="container-col">
-          ET Rate (per pay period):
-        </Col>
-        <Col>{summary.et_rate} hrs</Col>
-      </Row>
-      <Row>
-        <Col xs={8} md={8} className="container-col">
-          ET at End of Year:
-        </Col>
-        <Col>{summary.et_end_of_year} hrs</Col>
-      </Row>
-      <Row>
-        <Col xs={12} md={8} className="container-col">
-          ET/Holiday to Burn:
-        </Col>
-        <Col>{summary.et_hol_to_burn} hrs</Col>
-      </Row>
-      <Row>
-        <Col xs={8} md={8} className="container-col">
-          Total Vacation Weeks:
-        </Col>
-        <Col>{summary.total_vaca_weeks} week(s)</Col>
-      </Row>
-      <Row>
-        <Col xs={8} md={8} className="container-col">
-          Total Vacation Days:
-        </Col>
-        <Col>{summary.total_vaca_days} days</Col>
-      </Row> */}
     </>
   );
 }
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -10,43 +10,58 @@ import { modalType } from '../exports/Functions';
 // Styles
 import '../styles/Transaction.css';
 
-function Transactions({ transactions, setModalShow, setModalType }) {
+function Transactions({ transactions, summary, setModalShow, setModalType }) {
   // Shows the modal depending on the navigation button clicked
   function showModal(modalType) {
     setModalType(modalType);
     setModalShow(true);
   }
 
-  return (
-    <>
-      <h3 className="align-center">Transactions</h3>
-      <Button
-        className="transaction-btn"
-        size="sm"
-        variant="outline-primary"
-        block
-        onClick={() => showModal(modalType.transaction)}
-      >
-        Add Transaction
-      </Button>
-      <Table striped bordered hover variant="dark" size="sm">
-        <thead>
-          <tr>
-            <th className="align-center">Date</th>
-            <th>Time Used (hrs)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map((transaction) => (
-            <tr key={transaction.id}>
-              <td className="align-center">{transaction.date}</td>
-              <td>{transaction.time_used}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </>
-  );
+  function setTransactionComp() {
+    if (summary.et_rate > 0) {
+      return (
+        <>
+          <h3 className="align-center">Transactions</h3>
+          <Button
+            className="transaction-btn"
+            size="sm"
+            variant="outline-primary"
+            block
+            onClick={() => showModal(modalType.transaction)}
+          >
+            Add Transaction
+          </Button>
+          <Table striped bordered hover variant="dark" size="sm">
+            <thead>
+              <tr>
+                <th className="align-center">Date</th>
+                <th>Time Used (hrs)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td className="align-center">{transaction.date}</td>
+                  <td>{transaction.time_used}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h3 className="align-center">Transactions</h3>
+          <p className="align-center">
+            Please update your profile in order to start adding transactions.
+          </p>
+        </>
+      );
+    }
+  }
+
+  return setTransactionComp();
 }
 
 export default Transactions;
