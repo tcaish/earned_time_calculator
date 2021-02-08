1d89ef16c2c1886047216b05fb5aff8dae8c67f0
Timothy Caish <TimothyCaish@simventions.com>
2021-02-08 08:04:24 -0500

    added graphql api

---
+++ src/graphql/queries.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getEarnedTimeInfo = /* GraphQL */ `
+  query GetEarnedTimeInfo($id: ID!) {
+    getEarnedTimeInfo(id: $id) {
+      id
+      userId
+      carry_over_et
+      used_et
+      current_hol
+      hire_date_month
+      hire_date_day
+      hire_date_year
+      total_et_allowed
+      total_yearly_paychecks
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listEarnedTimeInfos = /* GraphQL */ `
+  query ListEarnedTimeInfos(
+    $filter: ModelEarnedTimeInfoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listEarnedTimeInfos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        userId
+        carry_over_et
+        used_et
+        current_hol
+        hire_date_month
+        hire_date_day
+        hire_date_year
+        total_et_allowed
+        total_yearly_paychecks
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
