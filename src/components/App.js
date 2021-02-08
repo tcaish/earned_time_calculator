f1c4ecdf64b41236863b6a616de4d2642ef664d1
Timothy Caish <timmycaish11@gmail.com>
2021-02-08 16:28:00 -0500

    profile information updating correctly now

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 // React
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Alert, Container, Row, Col } from 'react-bootstrap';
 
 // Amplify
 import { withAuthenticator, AmplifyAuthenticator } from '@aws-amplify/ui-react';
@@ -42,53 +42,63 @@ function App() {
   const [authState, setAuthState] = React.useState();
   const [user, setUser] = React.useState();
 
-  // Props for modal component
+  // Props for modal components
   const [modalShow, setModalShow] = useState(false);
   const [modalType, setModalType] = useState(0);
 
-  // Summary state
+  // Props for summary component
   const [summary, setSummary] = useState(initialSummaryState);
 
-  // Transactions state
+  // Props for transactions component
   const [transactions, setTransactions] = useState(initialTransactionsState);
 
-  // Profile state
+  // Props for profile component
   const [profile, setProfile] = useState(initialProfileState);
 
+  // Alert state
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertText, setAlertText] = useState('');
+
+  // This updates the authentication information upon load
+  // This fetches the earned time information for the user upon load
   useEffect(() => {
     onAuthUIStateChange((nextAuthState, authData) => {
       setAuthState(nextAuthState);
       setUser(authData);
 
-      fetchETInfo();
+      fetchEtInfo();
     });
   }, []);
 
   // Fetches all the earned time information for the current user
-  async function fetchETInfo() {
-    const apiData = await API.graphql({ query: getEarnedTimeInfo, variables: { id: '0' } });
+  async function fetchEtInfo() {
+    const apiData = await API.graphql({
+      query: getEarnedTimeInfo,
+      variables: { id: '0' }
+    });
 
     const etInfo = apiData.data.getEarnedTimeInfo;
-    //console.log(etInfo);
-
-    setProfile({
-      userId: etInfo.userId,
-      carry_over_et: etInfo.carry_over_et,
-      used_et: etInfo.used_et,
-      current_hol: etInfo.current_hol,
-      hire_date_month: etInfo.hire_date_month,
-      hire_date_day: etInfo.hire_date_day,
-      hire_date_year: etInfo.hire_date_year,
-      total_et_allowed: etInfo.total_et_allowed,
-      total_yearly_paychecks: etInfo.total_yearly_paychecks
-    });
+    setProfile({ ...etInfo });
   }
 
+  // Updates the earned time information for the user in the database
   async function updateEtInfo(theFormData) {
-    //if (!formData.name || !formData.description) return;
-    await API.graphql({ query: updateEarnedTimeInfoMutation, variables: { input: theFormData } });
-    //setNotes([ ...notes, formData ]);
-    setProfile({...theFormData});
+    await API.graphql({
+      query: updateEarnedTimeInfoMutation,
+      variables: { input: theFormData }
+    })
+      .then(res => {
+        setAlertText('Profile updated successfully!');
+        setModalShow(false);
+        setShowAlert(true);
+      })
+      .catch(err => {
+        alert(
+          'There was an error updating your profile: ' + JSON.stringify(err)
+        );
+      });
+
+    setProfile({ ...theFormData });
   }
 
   // Returns the correct modal for the navigation button pressed
@@ -97,19 +107,42 @@ function App() {
 
     switch (modalType) {
       case modalFuncType.profile:
-        modal = <ProfileModal profile={theProfile} updateprofile={updateEtInfo} show={modalShow} onHide={() => setModalShow(false)}/>;
+        modal = (
+          <ProfileModal
+            profile={theProfile}
+            updateProfile={updateEtInfo}
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        );
         break;
       case modalFuncType.logout:
-        modal = <LogoutModal show={modalShow} onHide={() => setModalShow(false)} />;
+        modal = (
+          <LogoutModal show={modalShow} onHide={() => setModalShow(false)} />
+        );
         break;
       case modalFuncType.settings:
-        modal = <SettingsModal show={modalShow} onHide={() => setModalShow(false)} />;
+        modal = (
+          <SettingsModal show={modalShow} onHide={() => setModalShow(false)} />
+        );
         break;
       case modalFuncType.transactions:
-        modal = <TransactionsModal show={modalShow} onHide={() => setModalShow(false)} />;
+        modal = (
+          <TransactionsModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        );
         break;
       default:
-        modal = <CustomModal title="Error" body="There was an error executing your request!" show={modalShow} onHide={() => setModalShow(false)} />;
+        modal = (
+          <CustomModal
+            title="Error"
+            body="There was an error executing your request!"
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+        );
         break;
     }
 
@@ -119,6 +152,19 @@ function App() {
   return authState === AuthState.SignedIn && user ? (
     <>
       <NavigationBar setModalShow={setModalShow} setModalType={setModalType} />
+
+      <Container>
+        {showAlert && modalType === modalFuncType.profile && (
+          <Alert
+            className="align-center"
+            variant="success"
+            onClose={() => setShowAlert(false)}
+            dismissible
+          >
+            {alertText}
+          </Alert>
+        )}
+      </Container>
 
       <Container id="app-container">
         <Row>
@@ -137,9 +183,7 @@ function App() {
       </Container>
 
       {/* Modals for settings, profile, and logout */}
-      {
-        getModal(profile)
-      }
+      {profile.userId !== '' && getModal(profile)}
     </>
   ) : (
     <AmplifyAuthenticator />
