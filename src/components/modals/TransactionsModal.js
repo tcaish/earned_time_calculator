a60366407b4b8fcf244078a88277aee9b7104814
Timothy Caish <TimothyCaish@simventions.com>
2021-02-11 13:26:52 -0500

    added transaction modal code...still needs work

--- src/components/modals/TransactionsModal.js
+++ src/components/modals/TransactionsModal.js
@@ -1,7 +1,22 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
+
 // React bootstrap
-import { Button, Modal } from 'react-bootstrap';
+import {
+  Alert,
+  Button,
+  ButtonGroup,
+  Col,
+  Form,
+  Modal,
+  Row,
+  ToggleButton
+} from 'react-bootstrap';
+
+// Third-party
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
 // Styles
 import '../../styles/CustomModal.css';
 
@@ -12,32 +27,201 @@ import '../../styles/CustomModal.css';
 @params props The "show" and "onHide" props.
 */
 function TransactionsModal(props) {
+  // Form state
+  const [formData, setFormData] = useState({ ...props.profile });
+
+  // Alert state
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertText, setAlertText] = useState('');
+
+  // Radio button states
+  const [typeRadioValue, setTypeRadioValue] = useState('Earned Time');
+  const [debitRadioValue, setDebitRadioValue] = useState('true');
+
+  const types = [
+    { name: 'Earned Time', value: 'Earned Time' },
+    { name: 'Holiday', value: 'Holiday' }
+  ];
+
+  const debits = [
+    { name: 'Withdrawl', value: 'true' },
+    { name: 'Deposit', value: 'false' }
+  ];
+
+  // Adds a transaction to the database for a user
+  function addTransaction(e) {
+    e.preventDefault();
+
+    // If all fields aren't filled in
+    if (
+      !formData.date ||
+      !formData.debit ||
+      !formData.time_used ||
+      !formData.type
+    ) {
+      setAlertText('Please fill in the required fields!');
+      setShowAlert(true);
+      return;
+    }
+
+    console.log(formData);
+    // Remove these properties because they'll cause the mutation to
+    // error out
+    delete formData.createdAt;
+    delete formData.updatedAt;
+    delete formData.owner;
+
+    props.addTransaction({ ...formData });
+  }
+
   return (
-    <Modal
-      {...props}
-      className="custom-modal"
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add New Transaction
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Transaction header</h4>
-        <p>Let's add a new transaction</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.onHide}>
-          Close
-        </Button>
-        <Button variant="success" onClick={props.onHide}>
-          Update
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Modal
+        show={props.show}
+        onHide={props.onHide}
+        className="custom-modal"
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Add New Transaction
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {showAlert && (
+            <Alert
+              className="align-center"
+              variant="warning"
+              onClose={() => setShowAlert(false)}
+              dismissible
+            >
+              {alertText}
+            </Alert>
+          )}
+
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group controlId="formTransactionType">
+                  <Form.Label>Type</Form.Label>
+                  <br />
+                  <ButtonGroup toggle>
+                    {types.map((type, idx) => (
+                      <ToggleButton
+                        key={idx}
+                        type="radio"
+                        variant="primary"
+                        name="radio"
+                        value={type.value}
+                        checked={typeRadioValue === type.value}
+                        onChange={e => {
+                          setTypeRadioValue(e.currentTarget.value);
+                          setFormData({
+                            ...formData,
+                            type: e.currentTarget.value
+                          });
+                        }}
+                      >
+                        {type.name}
+                      </ToggleButton>
+                    ))}
+                  </ButtonGroup>
+                  <Form.Text className="text-muted">
+                    The type of transaction to add.
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formTransactionDate">
+                  <Form.Label>Date</Form.Label>
+                  <br />
+                  <DatePicker
+                    selected={new Date()}
+                    onChange={theDate =>
+                      setFormData({ ...formData, date: theDate })
+                    }
+                  />
+                  <Form.Text className="text-muted">
+                    The date of the transaction.
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Form.Group controlId="formTransactionDebit">
+                  <Form.Label>Transaction</Form.Label>
+                  <br />
+                  <ButtonGroup toggle>
+                    {debits.map((type, idx) => (
+                      <ToggleButton
+                        key={idx}
+                        type="radio"
+                        variant="info"
+                        name="radio"
+                        value={type.value}
+                        checked={debitRadioValue === type.value}
+                        onChange={e => {
+                          setDebitRadioValue(e.currentTarget.value);
+                          setFormData({
+                            ...formData,
+                            debit: e.currentTarget.value
+                          });
+                        }}
+                      >
+                        {type.name}
+                      </ToggleButton>
+                    ))}
+                  </ButtonGroup>
+                  <Form.Text className="text-muted">
+                    What you're doing with your earned time or holiday hours.
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group controlId="formHours">
+                  <Form.Label>Hours</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="0.0"
+                    autoComplete="off"
+                    value={formData.time_used}
+                    onClick={e => e.target.select()}
+                    onChange={e =>
+                      setFormData({
+                        ...formData,
+                        time_used: e.target.value
+                      })
+                    }
+                  />
+                  <Form.Text className="text-muted">
+                    The amount of hours you used or are adding.
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={e => addTransaction(e)}
+              block
+            >
+              Add Transaction
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.onHide}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
