e8473c010138a36131b5a73709c6295d2c10ec7d
Timothy Caish <TimothyCaish@simventions.com>
2021-02-17 08:42:46 -0500

    added capability to autohide alerts after specified time; alerts stack on top of each other

--- src/components/AlertToast.js
+++ src/components/AlertToast.js
@@ -1,10 +1,17 @@
 // React
-import React from 'react';
-import { Container, Toast } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Toast } from 'react-bootstrap';
 
 function AlertToast({ title, message }) {
+  const [showToast, setShowToast] = useState(true);
+
   return (
-    <Toast>
+    <Toast
+      show={showToast}
+      onClose={() => setShowToast(false)}
+      delay={5000}
+      autohide
+    >
       <Toast.Header>
         {/* <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" /> */}
         <strong className="mr-auto">{title}</strong>
